2d941313d861c4049056b2211d270f5a436dd965
anelkhvan <36185294+anelkhvan@users.noreply.github.com>
2021-06-05 19:19:17 +0900

    Reverting config_path to the original

--- parlai/chat_service/services/browser_chat/run.py
+++ parlai/chat_service/services/browser_chat/run.py
@@ -47,7 +47,7 @@ def run(opt):
 
 if __name__ == '__main__':
     opt = setup_args()
-    config_path = opt.get('parlai/chat_service/tasks/qa_data_collection/config.yml')
+    config_path = opt.get('config_path')
     config = config_utils.parse_configuration_file(config_path)
     opt.update(config['world_opt'])
     opt['config'] = config
